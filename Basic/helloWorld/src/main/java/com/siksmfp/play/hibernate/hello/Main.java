904e198eccd4a56799684db16aa7afd5860ba047
parkito <artyom-karnov@yandex.ru>
2018-11-05 01:12:12 +0300

    Add prsisting

--- Basic/helloWorld/src/main/java/com/siksmfp/play/hibernate/hello/Main.java
+++ Basic/helloWorld/src/main/java/com/siksmfp/play/hibernate/hello/Main.java
@@ -1,8 +1,28 @@
 package com.siksmfp.play.hibernate.hello;
 
+import com.siksmfp.play.hibernate.hello.entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldPU");
+        EntityManager em = emf.createEntityManager();
 
+        em.getTransaction().begin();
+
+        User user = new User();
+        user.setName("new");
+        user.setSecondName("new");
+
+        em.persist(user);
+        em.getTransaction().commit();
+        em.close();
+
+        System.out.println("Finish");
+        emf.close();
     }
 }
--- Basic/helloWorld/src/main/java/com/siksmfp/play/hibernate/hello/entity/User.java
+++ Basic/helloWorld/src/main/java/com/siksmfp/play/hibernate/hello/entity/User.java
@@ -1,4 +1,45 @@
 package com.siksmfp.play.hibernate.hello.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usr")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "second_name")
+    private String secondName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
 }
